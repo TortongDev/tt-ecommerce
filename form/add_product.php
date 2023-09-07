088e41cb8571c8691f7a0dd1f43669d4460cb7a8
tortongDev <kittithat.charoenkhet101@gmail.com>
2023-09-07 01:34:59 -0700

    addd

--- form/add_product.php
+++ form/add_product.php
@@ -2,14 +2,14 @@
 
     require_once "../services/class/Connection.php";
     $db = new Connection(true);
-    $product_id         = isset($_POST['product_id']) ? htmlspecialchars(trim($_POST['product_id'])) : '';
-    $product_name       = isset($_POST['product_name']) ? htmlspecialchars(trim($_POST['product_name'])) : '';
-    $product_type       = isset($_POST['product_type']) ? htmlspecialchars(trim($_POST['product_type'])) : '';
-    $product_detail     = isset($_POST['product_detail']) ? htmlspecialchars(trim($_POST['product_detail'])) : '';
-    $product_sub_detail = isset($_POST['product_sub_detail']) ? htmlspecialchars(trim($_POST['product_sub_detail'])) : '';
-    $product_img        = isset($_POST['product_img']) ? htmlspecialchars(trim($_POST['product_img'])) : '';
-    $product_amount     = isset($_POST['product_amount']) ? htmlspecialchars(trim($_POST['product_amount'])) : '';
-    $product_price      = isset($_POST['product_price']) ? htmlspecialchars(trim($_POST['product_price'])) : '';
+    // $product_id         = isset($_POST['product_id'])       ? htmlspecialchars(trim($_POST['product_id'])) : '';
+    $product_name       = isset($_POST['product_name'])     ? htmlspecialchars(trim($_POST['product_name'])) : '';
+    $product_price      = isset($_POST['product_type'])     ? htmlspecialchars(trim($_POST['product_type'])) : '';
+    $product_amount     = isset($_POST['product_detail'])   ? htmlspecialchars(trim($_POST['product_detail'])) : '';
+    $product_user_id    = isset($_POST['product_sub_detail']) ? htmlspecialchars(trim($_POST['product_sub_detail'])) : '';
+    $product_img        = isset($_POST['product_img'])      ? htmlspecialchars(trim($_POST['product_img'])) : '';
+    $product_detail     = isset($_POST['product_amount'])   ? htmlspecialchars(trim($_POST['product_amount'])) : '';
+    $product_sub_detail = isset($_POST['product_price'])    ? htmlspecialchars(trim($_POST['product_price'])) : '';
 
     
     // if(!empty($product_amount)): 
@@ -21,6 +21,25 @@
 
     // endif;
 
-    $insertStmtProduct = $db->pdo->prepare("");
-    $insertStmtProduct->execute(array());
+    $insertStmtProduct = $db->pdo->prepare("INSERT INTO `kanji_products`
+                                                ( 
+                                                    `product_name`, 
+                                                    `product_price`, 
+                                                    `product_amount`, 
+                                                    `product_user_id`, 
+                                                    `product_img`, 
+                                                    `product_detail`, 
+                                                    `product_sub_detail`
+                                                ) VALUES (?,?,?,?,?,?,?)");
+    $insertStmtProduct->execute(
+        array(
+            $product_name,
+            $product_price,
+            $product_amount,
+            $product_user_id,
+            $product_img,
+            $product_detail,
+            $product_sub_detail
+        )
+    );
 ?>
